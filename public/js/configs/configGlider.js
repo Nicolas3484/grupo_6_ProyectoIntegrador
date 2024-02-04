dbe484d33934e311b5b5ec3146f911234d55e31f
Nicolas3484 <gonzaleznico08@gmail.com>
2024-02-04 20:36:04 -0300

    Terminado index.html +css +glider +bootstrap

---
+++ public/js/configs/configGlider.js
@@ -0,0 +1,33 @@
+new Glider(document.querySelector('.glider'), {
+    // Mobile-first defaults
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    scrollLock: true,
+    dots: '#resp-dots',
+    arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next'
+    },
+    responsive: [
+      {
+        // screens greater than >= 368px
+        breakpoint: 368,
+        settings: {
+          // Set to `auto` and provide item width to adjust to viewport
+          slidesToShow: '2.5',
+          slidesToScroll: '1',
+          itemWidth: 150,
+          duration: 0.25
+        }
+      },{
+        // screens greater than >= 769px
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 3.5,
+          slidesToScroll: 1,
+          itemWidth: 150,
+          duration: 0.25
+        }
+      }
+    ]
+  });
