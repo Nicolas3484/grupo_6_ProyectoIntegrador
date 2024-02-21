e4b7dd7eb46f906c428433b8e8357ae38e0482e9
Agustina-Gonzalez20 <ramepirane@gmail.com>
2024-02-21 01:10:44 -0300

    Separo los componentes repetidos en archivos parciales

--- app.js
+++ app.js
@@ -8,22 +8,22 @@ app.use(express.static('public'))
 
 // Rutas
 app.get('/',(req,res) => {
-    res.sendFile(path.join(__dirname,'./views/index.html'));
+    res.sendFile(path.join(__dirname,'./views/index.ejs'));
   })
   app.get("/home", (req, res) => {
     res.redirect('/');
   });
   app.get('/register',(req,res) => {
-    res.sendFile(path.join(__dirname,'./views/register.html'));
+    res.sendFile(path.join(__dirname,'./views/register.ejs'));
   })
   app.get('/login',(req,res) => {
-    res.sendFile(path.join(__dirname,'./views/login.html'));
+    res.sendFile(path.join(__dirname,'./views/login.ejs'));
   })
   app.get('/Producto',(req,res) => {
-    res.sendFile(path.join(__dirname,'./views/productoDetail.html'));
+    res.sendFile(path.join(__dirname,'./views/productoDetail.ejs'));
   })
   app.get('/productCart',(req,res) => {
-    res.sendFile(path.join(__dirname,'./views/productCart.html'));
+    res.sendFile(path.join(__dirname,'./views/productCart.ejs'));
   })
 // Server
 app.listen(port,() => console.log(`http://localhost:${port}`));
