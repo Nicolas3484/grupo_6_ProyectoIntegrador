6fcb07685a4671b5fe4c39a54f9680598a12b2a4
Agustina-Gonzalez20 <ramepirane@gmail.com>
2024-02-23 23:29:41 -0300

    correciones del Primer sprint

--- app.js
+++ app.js
@@ -5,25 +5,27 @@ const port = 3030;
 
 // Configs
 app.use(express.static('public'))
+app.set("view engine","ejs")
+app.set("views",path.join(__dirname,"./views"))
 
 // Rutas
 app.get('/',(req,res) => {
-    res.sendFile(path.join(__dirname,'./views/index.ejs'));
+    res.render(path.join(__dirname,'./views/index.ejs'));
   })
   app.get("/home", (req, res) => {
-    res.redirect('/');
+    res.render('/');
   });
   app.get('/register',(req,res) => {
-    res.sendFile(path.join(__dirname,'./views/register.ejs'));
+    res.render(path.join(__dirname,'./views/register.ejs'));
   })
   app.get('/login',(req,res) => {
-    res.sendFile(path.join(__dirname,'./views/login.ejs'));
+    res.render(path.join(__dirname,'./views/login.ejs'));
   })
   app.get('/Producto',(req,res) => {
-    res.sendFile(path.join(__dirname,'./views/productoDetail.ejs'));
+    res.render(path.join(__dirname,'./views/productoDetail.ejs'));
   })
   app.get('/productCart',(req,res) => {
-    res.sendFile(path.join(__dirname,'./views/productCart.ejs'));
+    res.render(path.join(__dirname,'./views/productCart.ejs'));
   })
 // Server
 app.listen(port,() => console.log(`http://localhost:${port}`));
