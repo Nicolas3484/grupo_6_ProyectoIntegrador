682ad2c6134dc62b7dcc72cc4fcd3d67a0e5103c
Pablo Caneda <canedapablomartin@gmail.com>
2024-04-03 18:29:32 -0300

    Cambios en general

--- src/routes/authentication.routes.js
+++ src/routes/authentication.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { login, register, logginProcces, registerProcess } = require("../controllers/autentication");
 const {registerValidation } = require("../middlewares/validations/authvalidation");
 const {loginValidation } = require("../middlewares/validations/loginValidation");
-
+const userProfile = require("../controllers/autentication/userProfile");
 // Rutas
 router.get("/iniciar", login);
 router.post("/iniciar", loginValidation, logginProcces); 
@@ -11,5 +11,7 @@ router.post("/iniciar", loginValidation, logginProcces);
 router.get("/registrar", register);
 
 router.post("/registrar", registerValidation, registerProcess)
+
+router.get("/perfil", userProfile)
 
 module.exports = router;
