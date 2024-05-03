174cdac76be9935ef704fdda1fd525ee62c9f604
Agustina-Gonzalez20 <ramepirane@gmail.com>
2024-05-02 21:57:53 -0300

    Agrego migraciones-modelos de productos y usuarios

---
+++ src/database/migrations/20240502193046-create-product.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const { toDefaultValue } = require('sequelize/lib/utils');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      titulo: {
+        type: Sequelize.STRING
+      },
+      subtitulo: {
+        type: Sequelize.STRING
+      },
+      precio: {
+        type: Sequelize.INTEGER
+      },
+      descripcion: {
+        type: Sequelize.TEXT
+      },
+      autores: {
+        type: Sequelize.STRING
+      },
+      imagen: {
+        type: Sequelize.STRING
+      },
+      disponible: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('products');
+  }
+};
