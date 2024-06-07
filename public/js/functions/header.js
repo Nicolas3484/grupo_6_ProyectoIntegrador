e31c5cc8ccddcd6df3efef086a22aab8d5cf0a4e
tinchoaraneda <martin.araneda@outlook.com>
2024-06-07 02:14:10 -0300

    Reestructuracion de carpetas

--- public/js/functions/header.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const menuToggle = document.getElementById('mobile-menu');
-  const navList = document.querySelector('.nav-list');
-
-  menuToggle.addEventListener('click', () => {
-      navList.classList.toggle('active');
-      menuToggle.classList.toggle('active'); // Agregado para cambiar el icono
-  });
-});
