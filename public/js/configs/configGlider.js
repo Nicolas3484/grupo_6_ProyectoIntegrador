b7bd28e935ac146ec4f2d8e8efc207cdb71b1165
Nicolas3484 <gonzaleznico08@gmail.com>
2024-07-17 18:18:51 -0300

    asdasdadasd

--- public/js/configs/configGlider.js
+++ public/js/configs/configGlider.js
@@ -1,31 +1,34 @@
 new Glider(document.querySelector('.glider'), {
-    // Mobile-first defaults
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    scrollLock: true,
-    dots: '#resp-dots',
-    draggable: true,
-    responsive: [
-      {
-        // screens greater than >= 368px
-        breakpoint: 368,
-        settings: {
-          // Set to `auto` and provide item width to adjust to viewport
-          slidesToShow: '2.5',
-          slidesToScroll: '1',
-          itemWidth: 150,
-          duration: 0.25,
-        }
-      },{
-        // screens greater than >= 769px
-        breakpoint: 769,
-        settings: {
-          slidesToShow: 3.5,
-          slidesToScroll: 1,
-          itemWidth: 150,
-          duration: 0.25,
-          
-        }
+  // Mobile-first defaults
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  scrollLock: true,
+  draggable: true,
+  dots: '#resp-dots',
+  arrows: {
+    prev: '.glider-prev',
+    next: '.glider-next'
+  },
+  responsive: [
+    {
+      // screens greater than >= 775px
+      breakpoint: 775,
+      settings: {
+        // Set to `auto` and provide item width to adjust to viewport
+        slidesToShow: 'auto',
+        slidesToScroll: 'auto',
+        itemWidth: 150,
+        duration: 0.25
       }
-    ]
-  });
+    },{
+      // screens greater than >= 1024px
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        itemWidth: 150,
+        duration: 0.25
+      }
+    }
+  ]
+});
